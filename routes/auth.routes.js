598816a5e166569d276e7155c1328c0d998f858d
rivesh25 <riveshkumar2004@gmail.com>
2026-03-08 17:42:45 +0530

    Error handling and Authentication

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import { signUp, signIn, signOut } from '../controllers/auth.controller.js';
 
 const authRouter = Router();
 
-authRouter.post('/sign-up', (req, res) => res.send({ title: 'Sign Up' }));
-authRouter.post('/sign-in', (req, res) => res.send({ title: 'Sign In' }));
-authRouter.post('/sign-out', (req, res) => res.send({ title: 'Sign Out' }));
+authRouter.post('/sign-up', (req, res) => signUp);
+authRouter.post('/sign-in', (req, res) => signIn);
+authRouter.post('/sign-out', (req, res) => signOut);
 
 export default authRouter;

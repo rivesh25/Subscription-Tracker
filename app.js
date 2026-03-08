ed4d9e80100ced08ece409eae8cb375df3e9829f
rivesh25 <riveshkumar2004@gmail.com>
2026-03-08 17:03:29 +0530

    database setup

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { PORT } from './config/env.js';
 import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
+import connectToDatabase from './database/mongodb.js';
 
 const app = express();
 
@@ -14,8 +15,9 @@ app.get('/', (req, res) => {
     res.send('Welcome to Subscription Tracker API!');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Subscription Tracker API is running on port http://localhost:${PORT}`);
+    await connectToDatabase();
 });
 
 export default app;
